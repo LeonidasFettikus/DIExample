8a1808ebe3b79591ce21e8c0c156be207e38e5ab
Martin Teichler <Martin.Teichler@condato.de>
2018-06-15 11:39:20 +0200

    Add project files.

---
+++ WebApplication2/CacheService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebApplication2
+{
+    public class CacheService : HostedService
+    {
+        public CacheService()
+        {
+
+        }
+
+        protected override Task ExecuteAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebApplication2/ClientService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebApplication2
+{
+    public class ClientService : HostedService
+    {
+        private CacheService _cache;
+
+        public ClientService(CacheService cache)
+        {
+            _cache = cache;
+        }
+        protected override Task ExecuteAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
